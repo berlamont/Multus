3c145104006d9088d14e2d0c95b32879a0632dfa
Robert <Robert@DESKTOP-TDV0BUS>
2023-09-22 13:36:58 -0400

    Add project files.

---
+++ App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace Multus
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
